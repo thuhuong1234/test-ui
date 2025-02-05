9a9e468fd17097626809bc3f54e21faeb0392d44
thuhuong1234 <hdnguyenthuhuong1234@gmail.com>
2025-02-05 23:54:37 +0700

    Implement header for app

--- src/App.vue
+++ src/App.vue
@@ -1,5 +1,23 @@
-<script setup></script>
 <template>
-  <RouterView />
+  <div class="main-dashboard">
+    <AppHeader />
+    <main></main>
+    <AppFooter />
+  </div>
 </template>
-<style scoped></style>
+<script setup>
+import AppHeader from "./components/AppHeader.vue";
+</script>
+<style scoped>
+* {
+  margin: 0;
+  padding: 0;
+  box-sizing: border-box;
+}
+
+.main-dashboard {
+  width: 100vw;
+  height: 100vh;
+  background: linear-gradient(#007ff4, #a2d6ec);
+}
+</style>
---
+++ src/components/AppHeader.vue
@@ -0,0 +1,47 @@
+<template>
+    <header>
+        <div class="app-header">
+            <div class="app-menu">
+                <img class="menu-btn" :src="menuIcon" alt="Menu">
+                <div class="app-logo">
+                    <img class="logo" :src="logoIcon" alt="Logo">
+                </div>
+            </div>
+
+            <div class="app-account">
+                <img class="account" width="40px" height="40px" :src="defaultAvatar" alt="Account">
+            </div>
+        </div>
+    </header>
+</template>
+
+<script setup>
+import menuIcon from '@/assets/images/menu.svg';
+import logoIcon from '@/assets/images/logo.svg';
+import defaultAvatar from '@/assets/images/default-avatar.jpeg';
+</script>
+
+<style scoped>
+.app-header {
+    display: flex;
+    align-items: center;
+    justify-content: space-between;
+    padding: 10px 40px;
+}
+
+.app-menu {
+    display: flex;
+    gap: 50px;
+}
+
+.app-account {
+    border: 1px solid white;
+    border-radius: 50%;
+    width: 40px;
+    height: 40px;
+}
+
+.account {
+    border-radius: 50%;
+}
+</style>
